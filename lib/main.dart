b625abad2af7864febec05920fe4be5ca63d880a
muralimgwl <murali.m@goodworklabs.com>
2020-08-23 20:34:29 +0530

    how to use container

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,16 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Scaffold(backgroundColor: Colors.grey));
+    return MaterialApp(
+        home: Scaffold(
+            backgroundColor: Colors.grey,
+            body: SafeArea(
+                child: Container(
+              width: 100.0,
+              height: 100.0,
+              margin: EdgeInsets.fromLTRB(50.0, 30.0, 50.0, 30.0),
+              padding: EdgeInsets.only(left: 10.0),
+              child: Text('Test'),
+            ))));
   }
 }
