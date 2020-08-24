6dcbf37df923374582eae650e4c39bae9069cb37
muralimgwl <murali.m@goodworklabs.com>
2020-08-24 09:19:10 +0530

    added font, card and list style

--- lib/main.dart
+++ lib/main.dart
@@ -12,37 +12,55 @@ class MyApp extends StatelessWidget {
         home: Scaffold(
             backgroundColor: Colors.teal,
             body: SafeArea(
-                child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                Container(
-                  width: 100.0,
-                  color: Colors.red,
+                CircleAvatar(
+                  backgroundImage: NetworkImage(
+                      'https://firebasestorage.googleapis.com/v0/b/mihy-all.appspot.com/o/WhatsApp%20Image%202019-02-23%20at%209.37.56%20PM.jpeg?alt=media&token=fa3d29e1-7dc2-429e-89b1-b9aa677ea91d'),
+                  radius: 50.0,
                 ),
-                SizedBox(width: 20.0),
-                Container(
-                  width: 100.0,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Container(
-                        width: 100.0,
-                        height: 100.0,
-                        color: Colors.yellow,
-                      ),
-                      Container(
-                        width: 100.0,
-                        height: 100.0,
-                        color: Colors.green,
-                      )
-                    ],
+                Text("Tharu",
+                    style: TextStyle(
+                        fontSize: 40,
+                        color: Colors.white,
+                        fontFamily: 'Pacifico',
+                        fontWeight: FontWeight.bold)),
+                Text("Founder & CEO",
+                    style: TextStyle(
+                        fontSize: 24,
+                        color: Colors.teal.shade100,
+                        fontFamily: 'Source Sans Pro',
+                        fontWeight: FontWeight.bold,
+                        letterSpacing: 2.5)),
+                SizedBox(
+                  height: 8.0,
+                  width: 80.0,
+                  child: Divider(color: Colors.teal.shade100),
+                ),
+                Card(
+                  margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 24.0),
+                  color: Colors.white,
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.phone,
+                      color: Colors.teal,
+                    ),
+                    title: Text('+918867002948',
+                        style: TextStyle(
+                            fontFamily: 'Source Sans Pro', color: Colors.teal)),
                   ),
                 ),
-                SizedBox(width: 20.0),
-                Container(
-                  width: 100.0,
-                  color: Colors.blue,
-                ),
+                Card(
+                  margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 24.0),
+                  color: Colors.white,
+                  child: ListTile(
+                    leading: Icon(Icons.email, color: Colors.teal),
+                    title: Text('tharu971191@gmail.com',
+                        style: TextStyle(
+                            fontFamily: 'Source Sans Pro', color: Colors.teal)),
+                  ),
+                )
               ],
             ))));
   }
